0c02e732b6e43b4c6123593fb0f2533122f1ed55
JAGUARAVI <47686625+JAGUARAVI@users.noreply.github.com>
2020-10-02 23:18:49 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.bg2').hide(0);
+	$('.bg3').hide(0);
+});
+
+function LoopForever() {
+	
+}
+
+var interval = self.setInterval(function(){LoopForever()},6000);
+
+//call code bllow to stop interval
+//window.clearInterval(interval);
+
+//THIS IS CURRENTLY UNDER WORK AND IT DOESN'T EVEN CONTAIN ANY CODE
