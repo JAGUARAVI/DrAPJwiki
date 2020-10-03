bff27f18a63f8463e5a65c166d85d73622dfd348
JAGUARAVI <47686625+JAGUARAVI@users.noreply.github.com>
2020-10-03 09:28:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
+	$('.bg1').hide(0);
 	$('.bg2').hide(0);
-	$('.bg3').hide(0);
+	slideshow();
+
 });
 
-function LoopForever() {
-	
+function Slide1() {
+	$('.bg1').fadeIn(1000);
+	$('.bg2').fadeOut(1000);
+	$('.bg3').fadeOut(1000);
+}
+function Slide2() {
+	$('.bg2').fadeIn(1000);
+	$('.bg1').fadeOut(1000);
+	$('.bg3').fadeOut(1000);
+}
+function Slide3() {
+	$('.bg3').fadeIn(1000);
+	$('.bg1').fadeOut(1000);
+	$('.bg2').fadeOut(1000);
+}
+function slideshow() {
+	setTimeout(function(){Slide1();}, 4000);
+	setTimeout(function(){Slide2();}, 8000);
+	setTimeout(function(){Slide3();}, 12000);
+
 }
 
-var interval = self.setInterval(function(){LoopForever()},6000);
+
+var interval1 = self.setInterval(function(){slideshow()},12000);
+
 
 //call code bllow to stop interval
 //window.clearInterval(interval);
-
-//THIS IS CURRENTLY UNDER WORK AND IT DOESN'T EVEN CONTAIN ANY CODE
